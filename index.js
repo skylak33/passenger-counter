19fff3381ac8fa37e374c9ab8f889de91944a77c
skylak33 <guremorir@gmail.com>
2023-11-23 22:39:31 +0300

    adding

---
+++ index.js
@@ -0,0 +1,21 @@
+let welcomeEl = document.getElementById("welcome-el")
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("saved")
+welcomeEl.innerText = "Welcome back, " + "Ryo👋";
+let count = 0
+
+
+function increment() {
+    count += 1
+    countEl.innerText = count;
+    
+}
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+}
+function reset() {
+    count = 0
+    countEl.textContent = count
+    saveEl.textContent = ""
+}
